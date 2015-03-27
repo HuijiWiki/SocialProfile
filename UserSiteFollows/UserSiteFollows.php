9f217c531d76b0cb8f2070a619a8d214dc845d6b
prozeal <minyitang@MacBook-Air.local>
2015-03-27 03:21:30 -0500

    template

---
+++ UserSiteFollows/UserSiteFollows.php
@@ -0,0 +1,15 @@
+class UserSiteFollow{
+	public $user_id;
+	public $user_name;
+	public wiki_domain;
+
+	public function addUserSiteFollow($user, $wikidomain){
+		
+	}
+
+	public function deleteUserSiteFollow($user, $wikidomain){
+
+	}
+
+	public function getSiteCount($wikidomain){}
+}
