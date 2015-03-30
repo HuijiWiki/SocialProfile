804359452a28eb21a17ff66154d2e657e1ea9b10
root <guxi99@gmail.com>
2015-03-30 12:40:34 +0800

    remove dupe class

--- UserSiteFollows/user_site_follow.sql
+++ UserSiteFollows/user_site_follow.sql
@@ -1,13 +1,13 @@
-CREATE TABLE /*_*/user_site_follow{
+CREATE TABLE /*_*/user_site_follow(
 `f_id` int(11) PRIMARY KEY auto_increment,
 `f_user_id` int(5) unsigned NOT NULL default '0',
 `f_user_name` varchar(255) NOT NULL default '',
 `f_wiki_domain` varchar(255) NOT NULL default '',
 `f_date` datetime default NULL
-}/*$wgDBTableOptions*/;
-CREATE INDEX /*i*/f_wiki_domain ON /*_*/user_site_follow_table (`f_wiki_domain`);
-CREATE TABLE /*_*/site_follow_count{
+)/*$wgDBTableOptions*/;
+CREATE INDEX if_wiki_domain ON /*_*/user_site_follow (`f_wiki_domain`);
+CREATE TABLE /*_*/user_site_follow_count(
 `f_wiki_domain` varchar(255) NOT NULL default '',
 `f_wiki_count` int(11) NOT NULL default '0'
-}/*$wgDBTableOptions*/;
-CREATE INDEX /*i*/f_wiki_domain ON /*_*/site_follow_count (`f_wiki_domain`);
+)/*$wgDBTableOptions*/;
+CREATE INDEX /*i*/f_wiki_domain ON /*_*/user_site_follow_count (`f_wiki_domain`);
