c8cc85c6e358edae6da43ce9cc226ee7a056ac9e
root <guxi99@gmail.com>
2015-09-15 16:29:23 +0800

    rm huiji.collectpageviewrecord

--- UserProfile/UserProfile.php
+++ UserProfile/UserProfile.php
@@ -83,7 +83,6 @@ $wgResourceModules['ext.socialprofile.userprofile.js'] = array(
 	'scripts' => array(
 	    '../UserUploadAvatar/cropper.min.js',
         '../UserUploadAvatar/main.js',
-        'huiji.collectPageViewRecord.js',
         'UserProfilePage.js'
 	),
 	'localBasePath' => __DIR__,
