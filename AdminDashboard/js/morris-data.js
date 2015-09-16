9d120e6435daaca1492d166ecfd246e713f9bbf7
root <guxi99@gmail.com>
2015-09-16 16:05:20 +0800

    1-4

--- AdminDashboard/js/morris-data.js
+++ AdminDashboard/js/morris-data.js
@@ -16,6 +16,7 @@ jQuery( document ).ready( function() {
                     'echarts/chart/line', // 使用柱状图就加载bar模块，按需加载
                     'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
                 ],
+                //1
                 function (ec) {
                     // 基于准备好的dom，初始化echarts图表
                     var  myChart = ec.init(document.getElementById('morris-area-echart'));
@@ -24,7 +25,7 @@ jQuery( document ).ready( function() {
                                 trigger: 'axis'
                             },
                             legend: {
-                                data:['网站得分','关注人数','浏览次数','编辑次数']
+                                data:['网站得分']
                             },
                             toolbox: {
                                 show : true,
@@ -57,14 +58,14 @@ jQuery( document ).ready( function() {
                                     // stack: '总量',
                                     // color:'red',
                                     // line style
-                                    // itemStyle:{
-                                    //     normal:{
-                                    //         lineStyle:{
-                                    //             color:'black',
-                                    //             width:3
-                                    //         }
-                                    //     }
-                                    // },
+                                    itemStyle:{
+                                        normal:{
+                                            lineStyle:{
+                                                color:'black',
+                                                width:3
+                                            }
+                                        }
+                                    },
                                     data:[],
 
                                     // 系列中的数据标注内容 series.markPoint  
@@ -75,97 +76,17 @@ jQuery( document ).ready( function() {
                                         ]  
                                     },  
                                     //系列中的数据标线内容 series.markLine  
-                                    markLine:{  
-                                        data:[  
-                                            {type:'average',name:'平均值'}  
-                                        ]  
-                                    }  
-                                },
-                                {
-                                    name:'关注人数',
-                                    type:'line',
-                                    // stack: '总量',
-                                    data:[],
-                                    // 系列中的数据标注内容 series.markPoint
-                                    markPoint:{
-                                        data:[
-                                            {type:'max',name:'最大值'},
-                                            {type:'min',name:'最小值'}
-                                        ]
-                                    },
-                                    //系列中的数据标线内容 series.markLine
-                                    markLine:{
-                                        data:[
-                                            {type:'average',name:'平均值'}
-                                        ]
-                                    }
-                                },
-                                {
-                                    name:'浏览次数',
-                                    type:'line',
-                                    // stack: '总量',
-                                    data:[],
-                                    //系列中的数据标注内容 series.markPoint
-                                    markPoint:{
-                                        data:[
-                                            {type:'max',name:'最大值'},
-                                            {type:'min',name:'最小值'}
-                                        ]
-                                    },
-                                    //系列中的数据标线内容 series.markLine
-                                    markLine:{
-                                        data:[
-                                            {type:'average',name:'平均值'}
-                                        ]
-                                    }
-                                },
-                                {
-                                    name:'编辑次数',
-                                    type:'line',
-                                    // stack: '总量',
-                                    data:[],
-                                    //系列中的数据标注内容 series.markPoint
-                                    markPoint:{
-                                        data:[
-                                            {type:'max',name:'最大值'},
-                                            {type:'min',name:'最小值'}
-                                        ]
-                                    },
-                                    //系列中的数据标线内容 series.markLine
-                                    markLine:{
-                                        data:[
-                                            {type:'average',name:'平均值'}
-                                        ]
-                                    }
+                                    // markLine:{  
+                                    //     data:[  
+                                    //         {type:'average',name:'平均值'}  
+                                    //     ]  
+                                    // }  
                                 }
                             ]
                         };
                     // };
                     // 为echarts对象加载数据
                     
-                    //all pv
-                    var site = mw.config.get('wgHuijiPrefix');
-                    huiji.getPreviousViewRecords(site,30,updateData);
-                    function updateData(data){
-                         if (data.status == 'success'){
-                            console.log(data.result);
-                            option.xAxis[0].data=data.result.date_array;
-                            option.series[2].data=data.result.number_array;
-                            myChart.setOption(option,false);
-                        }
-                    }
-
-                    //all pe
-                    huiji.getPreviousEditRecords(site,30,updateDatape);
-                    function updateDatape(data){
-                         if (data.status == 'success'){
-                            console.log(data.result);
-                            var res = data.result;
-                            option.xAxis[0].data=data.result.date_array;
-                            option.series[3].data=data.result.number_array;
-                            myChart.setOption(option,false);
-                        }
-                    }
 
                     //site rank
                     jQuery.post(
@@ -185,6 +106,75 @@ jQuery( document ).ready( function() {
                         }
                     );
 
+                }
+               
+            );
+            require(
+                [
+                    'echarts',
+                    'echarts/chart/line', // 使用柱状图就加载bar模块，按需加载
+                    'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
+                ],
+                //2
+                function (ec2) {
+                    // 基于准备好的dom，初始化echarts图表
+                    var  myChart = ec2.init(document.getElementById('morris-area-echart2'));
+                    var  option = {
+                            tooltip : {
+                                trigger: 'axis'
+                            },
+                            legend: {
+                                data:['关注人数']
+                            },
+                            toolbox: {
+                                show : true,
+                                feature : {
+                                    mark : {show: false},
+                                    dataView : {show: true, readOnly: true},
+                                    magicType : {show: true, type: ['line', 'bar']},
+                                    // magicType : {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+                                    restore : {show: true},
+                                    saveAsImage : {show: true}
+                                }
+                            },
+                            calculable : true,
+                            xAxis : [
+                                {
+                                    type : 'category',
+                                    boundaryGap : false,
+                                    data : []
+                                }
+                            ],
+                            yAxis : [
+                                {
+                                    type : 'value'
+                                }
+                            ],
+                            series : [
+                                {
+                                    name:'关注人数',
+                                    type:'line',
+                                    // stack: '总量',
+                                    data:[],
+                                    // 系列中的数据标注内容 series.markPoint
+                                    markPoint:{
+                                        data:[
+                                            {type:'max',name:'最大值'},
+                                            {type:'min',name:'最小值'}
+                                        ]
+                                    },
+                                    //系列中的数据标线内容 series.markLine
+                                    // markLine:{
+                                    //     data:[
+                                    //         {type:'average',name:'平均值'}
+                                    //     ]
+                                    // }
+                                }
+                            ]
+                        };
+                    // };
+                    // 为echarts对象加载数据
+
                     //site follow count
                     jQuery.post(
                         mw.util.wikiScript(), {
@@ -197,13 +187,178 @@ jQuery( document ).ready( function() {
                             console.log(res.result);
                             if ( res.success ){
                                 option.xAxis[0].data=res.result.date;
-                                option.series[1].data=res.result.FollowCount;
+                                option.series[0].data=res.result.FollowCount;
                                 myChart.setOption(option,false);
                             }
                         }
                     );
 
                 }
+               
+            );
+            require(
+                [
+                    'echarts',
+                    'echarts/chart/line', // 使用柱状图就加载bar模块，按需加载
+                    'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
+                ],
+                //3
+                function (ec3) {
+                    // 基于准备好的dom，初始化echarts图表
+                    var  myChart = ec3.init(document.getElementById('morris-area-echart3'));
+                    var  option = {
+                            tooltip : {
+                                trigger: 'axis'
+                            },
+                            legend: {
+                                data:['浏览次数']
+                            },
+                            toolbox: {
+                                show : true,
+                                feature : {
+                                    mark : {show: false},
+                                    dataView : {show: true, readOnly: true},
+                                    magicType : {show: true, type: ['line', 'bar']},
+                                    // magicType : {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+                                    restore : {show: true},
+                                    saveAsImage : {show: true}
+                                }
+                            },
+                            calculable : true,
+                            xAxis : [
+                                {
+                                    type : 'category',
+                                    boundaryGap : false,
+                                    data : []
+                                }
+                            ],
+                            yAxis : [
+                                {
+                                    type : 'value'
+                                }
+                            ],
+                            series : [
+                                {
+                                    name:'浏览次数',
+                                    type:'line',
+                                    // stack: '总量',
+                                    data:[],
+                                    //系列中的数据标注内容 series.markPoint
+                                    markPoint:{
+                                        data:[
+                                            {type:'max',name:'最大值'},
+                                            {type:'min',name:'最小值'}
+                                        ]
+                                    },
+                                    //系列中的数据标线内容 series.markLine
+                                    // markLine:{
+                                    //     data:[
+                                    //         {type:'average',name:'平均值'}
+                                    //     ]
+                                    // }
+                                }
+                            ]
+                        };
+                    // };
+                    // 为echarts对象加载数据
+                    
+                    //all pv
+                    var site = mw.config.get('wgHuijiPrefix');
+                    huiji.getPreviousViewRecords(site,30,updateData);
+                    function updateData(data){
+                         if (data.status == 'success'){
+                            console.log(data.result);
+                            option.xAxis[0].data=data.result.date_array;
+                            option.series[0].data=data.result.number_array;
+                            myChart.setOption(option,false);
+                        }
+                    }
+
+                }
+
+            );
+            require(
+                [
+                    'echarts',
+                    'echarts/chart/line', // 使用柱状图就加载bar模块，按需加载
+                    'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
+                ],
+                //4
+                function (ec4) {
+                    // 基于准备好的dom，初始化echarts图表
+                    var  myChart = ec4.init(document.getElementById('morris-area-echart4'));
+                    var  option = {
+                            tooltip : {
+                                trigger: 'axis'
+                            },
+                            legend: {
+                                data:['编辑次数']
+                            },
+                            toolbox: {
+                                show : true,
+                                feature : {
+                                    mark : {show: false},
+                                    dataView : {show: true, readOnly: true},
+                                    magicType : {show: true, type: ['line', 'bar']},
+                                    // magicType : {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+                                    restore : {show: true},
+                                    saveAsImage : {show: true}
+                                }
+                            },
+                            calculable : true,
+                            xAxis : [
+                                {
+                                    type : 'category',
+                                    boundaryGap : false,
+                                    data : []
+                                }
+                            ],
+                            yAxis : [
+                                {
+                                    type : 'value'
+                                }
+                            ],
+                            series : [
+                                
+                                {
+                                    name:'编辑次数',
+                                    type:'line',
+                                    // stack: '总量',
+                                    data:[],
+                                    //系列中的数据标注内容 series.markPoint
+                                    markPoint:{
+                                        data:[
+                                            {type:'max',name:'最大值'},
+                                            {type:'min',name:'最小值'}
+                                        ]
+                                    },
+                                    //系列中的数据标线内容 series.markLine
+                                    // markLine:{
+                                    //     data:[
+                                    //         {type:'average',name:'平均值'}
+                                    //     ]
+                                    // }
+                                }
+                            ]
+                        };
+                    // };
+                    // 为echarts对象加载数据
+                    
+                    var site = mw.config.get('wgHuijiPrefix');
+                    //all pe
+                    huiji.getPreviousEditRecords(site,30,updateDatape);
+                    function updateDatape(data){
+                         if (data.status == 'success'){
+                            console.log(data.result);
+                            var res = data.result;
+                            option.xAxis[0].data=data.result.date_array;
+                            option.series[0].data=data.result.number_array;
+                            myChart.setOption(option,false);
+                        }
+                    }
+
+                }
+
             );
         });
 
