17aeca2d7e48b0ab314f849216f35091ce0a9178
root <guxi99@gmail.com>
2015-11-05 16:14:12 +0800

    add OauthLoginajax

---
+++ UserProfile/OauthLogin_AjaxFunctions.php
@@ -0,0 +1,66 @@
+<?php
+$wgAjaxExportList[] = 'wfCheckOauth';
+$wgAjaxExportList[] = 'wfAddInfoToOauth';
+$wgAjaxExportList[] = 'wfAddUserOauthCookie';
+
+//check is exist openid
+function wfCheckOauth( $openid, $type ){
+	$dbr = wfGetDB( DB_SLAVE );
+	$res = $dbr->select(
+		'oauth',
+		array(
+			'user_id'
+		),
+		array(
+			'open_id' => $openid,
+			'o_type' => $type
+		),
+		__METHOD__
+	);
+	$user_id = null;
+	if ($res){
+		foreach($res as $value){
+			$user_id = $value->user_id;
+		}
+	}
+	$result = array('success'=> true, 'result'=>$user_id );
+	$out = json_encode($result);
+	return $out;
+}
+
+//insert into oauth
+function wfAddInfoToOauth( $otype, $openid, $userid ){
+	$dbw = wfGetDB( DB_MASTER );
+	$res = $dbw->insert(
+		'oauth',
+		array(
+			'o_type' => $otype,
+			'open_id' => $openid,
+			'user_id' => $userid,
+		),
+		__METHOD__
+	);
+	$u = User::newFromId( $userid );
+	$u->setCookies();
+	// $u->setSession();
+	if($res){
+		$result = array('success'=> true, 'result'=>'1' );
+		$out = json_encode($result);
+	}
+	return $out;
+}
+
+//wfAddUserOauthCookie
+function wfAddUserOauthCookie( $user_id ){
+	// session_start();
+	if ( $user_id ){
+		$u = User::newFromId( $user_id );
+		$arr = array('wpName'=>$u->getName());
+		$u->setCookies($arr, null, true);
+		// $u->setCookies( );
+	}
+	// session_id() = $user_id;
+	$res = array('success' => true, 'result'=>'1' );
+	$out = json_encode($res);
+	return  $out;
+}
