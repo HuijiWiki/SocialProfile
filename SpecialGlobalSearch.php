52af7b0ce405992aeb5e549726d9420e994e1ae8
root <guxi99@gmail.com>
2015-12-10 18:19:35 +0800

    globalsearch page&i18n main

--- SpecialGlobalSearch.php
+++ SpecialGlobalSearch.php
@@ -33,6 +33,9 @@ class SpecialGlobalSearch extends SpecialPage {
 		$out = $this->getOutput();
 		$request = $this->getRequest();
 		$key = empty($request->getVal( 'key' ))?null:$request->getVal( 'key' );
+		$page = empty($request->getVal('page'))?1:$request->getVal('page');
+		$per_page = 10;
+		$star_page = $per_page*($page-1)+1;
 		// Set the page title, robot policies, etc.
 		$this->setHeaders();
 		$output = "<span>global search</span>";
@@ -41,10 +44,10 @@ class SpecialGlobalSearch extends SpecialPage {
 			<input class='mw-ui-button mw-ui-progressive' type='submit' value='搜索'>
 			</form>";
 		if ( !is_null($key) ) {
-			$resJson = QueryInterface::pageSearch($key,3,0);
+			$resJson = QueryInterface::pageSearch($key, $per_page, $star_page);
 			$resObj = json_decode($resJson);
 			$resCount = $resObj->hits;
-			$output .= "<div class=\"results-info\"><strong>".$resCount."</strong>条结果中的<strong>1~20</strong>条</div>
+			$output .= "<div class=\"results-info\"><strong>".$resCount."</strong>条结果中的<strong>".$star_page."<span>到</span>".($per_page*$page)."</strong>条</div>
 					<ul class=\"mw-search-results\">";
 			foreach ($resObj->sites as $value) {
 				$d=strtotime($value->timestamp);
@@ -59,6 +62,71 @@ class SpecialGlobalSearch extends SpecialPage {
 							</li>";
 			}
 			$output .= '</ul>';
+
+
+			$pcount = $resCount;
+			$numofpages = $pcount / $per_page;
+
+			$page_link = $this->getPageTitle();
+
+			if ( $numofpages > 1 ) {
+				$output .= '<div class="page-nav-wrapper"><nav class="page-nav pagination">';
+
+				if ( $page > 1 ) {
+					$output .= '<li>'.Linker::link(
+						$page_link,
+						'<span aria-hidden="true">&laquo;</span>',
+						array(),
+						array(
+							'key' => $key,
+							// 'rel_type' => $rel_type,
+							'page' => ( $page - 1 )
+						)
+					) . '</li>';
+				}
+
+				if ( ( $pcount % $per_page ) != 0 ) {
+					$numofpages++;
+				}
+				if ( $numofpages >= 9 && $page < $pcount ) {
+					$numofpages = 9 + $page;
+				}
+				// if ( $numofpages >= ( $total / $per_page ) ) {
+				// 	$numofpages = ( $total / $per_page ) + 1;
+				// }
+
+				for ( $i = 1; $i <= $numofpages; $i++ ) {
+					if ( $i == $page ) {
+						$output .= ( '<li class="active"><a href="#">'.$i.' <span class="sr-only">(current)</span></a></li>' );
+					} else {
+						$output .= '<li>' .Linker::link(
+							$page_link,
+							$i,
+							array(),
+							array(
+								'key' => $key,
+								'page' => $i
+							)
+						);
+					}
+				}
+
+				if ( ( $pcount - ( $per_page * $page ) ) > 0 ) {
+					$output .= '<li>' .
+						Linker::link(
+							$page_link,
+							'<span aria-hidden="true">&raquo;</span>',
+							array(),
+							array(
+								'key' => $key,
+								// 'rel_type' => $rel_type,
+								'page' => ( $page + 1 )
+							)
+						).'</li>';	
+				}
+
+				$output .= '</nav></div>';
+			}
 		}
 		$out->addHTML( $output );
 	}
