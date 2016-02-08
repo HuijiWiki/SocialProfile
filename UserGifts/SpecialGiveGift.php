1bdd4dc2759935b0b09a3fa0ac759aa0c30f9adf
Reasno <guxi99@gmail.com>
2016-02-08 22:55:48 +0800

    Update SpecialGiveGift.php

--- UserGifts/SpecialGiveGift.php
+++ UserGifts/SpecialGiveGift.php
@@ -70,7 +70,7 @@ class GiveGift extends SpecialPage {
 			$out->addHTML( $this->msg( 'g-error-message-login' )->plain() );
 		} else {
 			$gift = new UserGifts( $user->getName() );
-			if ( $gift->doesUserOwnGift($user->getID(), $request->getInt( 'gift_id' ) ){
+			if ( $gift->doesUserOwnGift($user->getID(), $request->getInt( 'gift_id' ) ) ){
 				$out->setPageTitle( $this->msg( 'g-error-title' )->plain() );
 				$out->addHTML( $this->msg( 'g-error-already-owned-gift' )->plain() );				
 			}  elseif ( Gifts::isAllowedToSendGift( $user->getID(), $request->getInt( 'gift_id' ) ) ){
