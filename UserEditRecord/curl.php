ea6728a00fec1c085f2e71ff8935ab49589ee0f4
reasno <guxi99@gmail.com>
2016-03-17 14:13:25 +0800

    test

---
+++ UserEditRecord/curl.php
@@ -0,0 +1,34 @@
+<?php
+
+class MyCURL{
+ 
+
+
+   
+
+   public static function postDataInJson($url, $data_string, $username, $pwd)
+{
+               
+                $header = array(
+                        'Content-Type: application/json',
+                        'Content-Length: '.strlen($data_string),
+                        );
+        $curl_opt_a = array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_POST => 1,
+                CURLOPT_POSTFIELDS =>$data_string,
+                CURLOPT_HTTPHEADER =>$header,
+                CURLOPT_USERPWD => $username.':'.$pwd,
+        );
+        $ch = curl_init();
+        curl_setopt_array($ch,$curl_opt_a);
+        $out = curl_exec($ch);
+        curl_close($ch);
+
+        return $out;
+}
+}
+
+?>
