02843192f1bbffc0de2543fbca7d4b248e470554
reasno <guxi99@gmail.com>
2016-04-01 23:06:43 +0800

    add files

---
+++ AdminDashboard/SpecialInviteUser.php
@@ -0,0 +1,113 @@
+<?php   
+/**
+* uploadfiles
+*/
+class SpecialInviteUser extends UnlistedSpecialPage{
+    
+    function __construct(){
+        parent::__construct( 'InviteUser' );
+    }
+
+    public function execute( $params ) {
+        global $wgUser, $wgHuijiPrefix, $wgLocalFileRepo, $wgContLang;
+        // Set the page title, robot policies, etc.
+        $this->setHeaders();
+        $out = $this->getOutput();
+        $request = $this->getRequest();
+        $prefix = $request->getVal('prefix');
+        $userName = $request->getVal('user');
+        $redirect = $wgHuijiPrefix;
+        $output = '';
+        // $out->addModuleScripts('ext.socialprofile.qqLogin.js');
+        // $out->addModuleStyles('ext.socialprofile.videos.css');
+        // $out->addModules( array(
+        //     'skins.bootstrapmediawiki.videohandler',
+        //     'ext.socialprofile.videos.js'
+        //     ) 
+        // );
+        if ( $prefix == null || $userName == null ) {
+            $output .= '<div class="alert alert-danger">您输入的参数不正确。请返回首页。</div>';
+            $out->addHTML( $output );
+            return false;
+        }
+        $site = WikiSite::newFromPrefix($prefix);
+        if ($site->getId() != null) {
+            $huijiUser = HuijiUser::newFromName( $userName );
+            if ($huijiUser->getId() != null ) {
+                $userStats = $huijiUser->getStats(false);
+                $siteStats = $site->getStats(false);
+                $userGroup = $huijiUser->getEffectiveGroups();
+                if ( in_array('sysop', $userGroup) ) {
+                    $group = '管理员';
+                }elseif ( in_array('bureaucrat', $userGroup) ) {
+                    $group = '行政员';
+                }else{
+                    $group = '编辑者';
+                }
+                // print_r($huijiUser->getEffectiveGroups());die();
+                // print_r(SpecialPage::getTitleFor('InviteUser'));die();
+                $output .= "<div><span class=\"invite-title\">各位大神和水友：</span>
+                            <p>我是<b>".$site->getName()."</b>的".$group.":".$userName."，目前已经在".$site->getName()."编辑了".$userStats['edits']."次。</p>
+                            <p>诚挚的邀请您加入".$site->getName()."的编辑组。</p>
+                            <p>关于".$site->getName()."</p>
+                            ".$site->getDescription()."。<br>
+                            自".substr($site->getDate(),0,10)."创立以来，已经有".$siteStats['pages']."个页面，".$siteStats['edits']."次编辑和".$siteStats['followers']."个编辑者。<br>
+                            参与编辑组的好处：<br>
+                            * 汇聚星星之火，让更多人发现和了解".$site->getName()."<br>
+                            * 驾驭强大的维基系统，构建你的专属世界<br>
+                            * 获得专属标识，邂逅与你频率相同的“机”友<br>
+                            * 无需前置审核，不受束缚的热情创造<br>
+                            把爱分享给世界，世界也将用爱回报。<br>
+                            //（灰色小字）纯粹兴趣分享，无任何经济回报<br>
+                            <form  class='complete-user-info'><p>快速注册</p>
+                            用户名:<input type='text' id='qqloginusername' placeholder='用户名' name='qqloginname'><br>
+                            密码:<input type='password' id='qqloginpassword'  placeholder=\"请输入密码\" name='qqloginpass'><br>
+                            邮箱:<input type='email' id='qqloginemail' placeholder=\"请输入邮箱\" name='qqloginemail'><br>
+                            <input id='redirect_url' type='hidden' value='".$redirect."' >
+                            <input id='inviter' type='hidden' value='".$userName."' >
+                            <input id='inviteuser' type='hidden' value=1 >";
+
+                if ( $wgUser->getID() == 0 || $wgUser->getName() == '' ) {
+                    $submitButton ="<div class='mw-ui-button  mw-ui-block mw-ui-constructive btn' data-loading-text='提交中...' id='qqConfirm'>提交</div>";
+                }else{
+                    $submitButton ="<div class='mw-ui-button  mw-ui-block mw-ui-constructive btn' data-loading-text='提交中...' id='qqConfirm' disabled>提交</div>";
+                }
+                $output .= "</form></div>";
+            }else{
+                $output .= '<h1>user输入有误</h1>';
+            }
+        }else{
+            $output .= '<div class="alert alert-danger">您输入的参数不正确。请返回首页。</div>';
+            $out->addHTML( $output );
+            return false;
+        }
+        $templateParser = new TemplateParser(  __DIR__ . '/pages' );
+        $output = $templateParser->processTemplate(
+            'invites',
+            array(
+                'siteName' => $site->getName(),
+                'duty' => $group,
+                'userName' => $userName,
+                'numberOfEditsByMe' => $userStats['edits'],
+                'siteDescription' => $site->getDescription(),
+                'siteTime' => substr($site->getDate(),0,10),
+                'numberOfPages' => $siteStats['pages'],
+                'numberOfEdits' => $siteStats['edits'],
+                'numberOfFollowers' => $siteStats['followers'],
+                'redirect' => $redirect,
+                'submitButton' => $submitButton
+
+
+            )
+        );
+        $out->addModuleStyles(array('ext.socialprofile.inviteuser.css', 'ext.socialprofile.userinfo.css', 'mediawiki.special.userlogin.signup.styles'));
+        $out->addModules(array('ext.socialprofile.inviteuser.js', 'mediawiki.cookie', 'ext.socialprofile.qqLogin.js'));
+        $out->addHTML( $output );
+
+    }
+
+
+
+}
+
+?>
---
+++ AdminDashboard/WikiDoctor.php
@@ -0,0 +1,96 @@
+<?php
+class WikiDoctor{
+	private $mSite;
+	private static $trackingCategory = array(
+		//'index-category' => '已索引页面',
+		//'noindex-category' => '不可索引页面',
+		'duplicate-args-category' => '调用重复模板参数的页面',
+		'expensive-parserfunction-category' => '有过多高开销解析器函数调用的页面',
+		'post-expand-template-argument-category' => '含有略过模板参数的页面',
+		'post-expand-template-inclusion-category' => '模板包含上限已经超过的页面',
+		// 'hidden-category-category' => '隐藏分类',
+		'broken-file-category' => '含有受损文件链接的页面',
+		'node-count-exceeded-category' => '页面的节点数超出限制',
+		'expansion-depth-exceeded-category' => '扩展深度超出限制的页面',
+		'syntaxhighlight-error-category' => '有语法高亮错误的页面',
+		'scribunto-common-error-category' => '有脚本错误的页面',
+		'scribunto-module-with-errors-category' => '有错误的Scribunto模块',
+	);
+	private static $linkReports = array(
+		'双重重定向',
+		'需要的页面',
+		'需要的模板',
+		'孤立页面',
+		'未分类分类',
+		'未分类模板',
+		'未分类页面',
+		'断链页面',
+		'未使用文件',
+		'未使用模板',
+		'受损重定向'
+	);
+	public function __construct(){
+	}
+	public function categoryCheck(){
+		foreach ( WikiDoctor::$trackingCategory as $key => $value){
+			$cat = Category::newFromName($value);
+			if (!$cat){
+				continue;
+			}
+			$num = $cat->getPageCount();
+			if ($num){
+				return array($value, $num);
+			}
+		}
+		return array('' ,  '');
+		
+		//Decision Tree;
+		//check double redirects
+		//check tracking categories
+		//check 
+
+	}
+	public function linkCheck(){
+		$key = array_rand( WikiDoctor::$linkReports );
+		return WikiDoctor::$linkReports[$key];
+		// shuffle(Category::$linkReports);
+		// foreach ($linkReports as $page){
+		// 	return $page;
+		// }
+	}
+	public function engagementCheck(){
+		global $wgHuijiPrefix;
+		$this->mSite = WikiSite::newFromPrefix($wgHuijiPrefix);
+		$rating = $this->mSite->getRating();
+		$score = $this->mSite->getScore();
+		if ($rating == 'A'){
+			if ($this->mSite->getScore() < 90){
+				return 'score';
+			}			
+		}
+		if ($rating == 'B'){
+			if ($this->mSite->getScore() < 80){
+				return 'score';
+			}			
+		}
+		if ($rating == 'C'){
+			if ($this->mSite->getScore() < 70){
+				return 'score';
+			}			
+		}
+		if ($rating == 'D'){
+			if ($this->mSite->getScore() < 60){
+				return 'score';
+			}			
+		}
+		$stats = $this->mSite->getStats();
+		$stats['followers'] *= 60;
+		$stats['articles'] *= 40;
+		$bottom = min ( $stats['followers'], $stats['edits'] , $stats['articles'] );
+		$bottomKey = min(array_keys($stats, $bottom));
+		return $bottomKey;
+	}
+
+}
+
+?>
