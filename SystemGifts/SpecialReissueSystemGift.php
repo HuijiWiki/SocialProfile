d54548067e761141c4f06ef02014fecc9fb9e936
c86jeff <zhangjiexin0101@gmail.com>
2016-05-05 19:30:39 +0800

    reissuesystemgift

---
+++ SystemGifts/SpecialReissueSystemGift.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+*  special page to add new festival gift
+*/
+class SpecialReissueSystemGift extends SpecialPage{
+	
+	function __construct(){
+
+		parent::__construct( 'ReissueSystemGift' );
+	
+	}
+
+	/**
+	 * Group this special page under the correct header in Special:SpecialPages.
+	 *
+	 * @return string
+	 */
+	
+	function getGroupName() {
+		return 'wiki';
+	}
+
+	/**
+	 * Show the special page
+	 *
+	 */
+	public function execute($params){
+		global $wgUser;
+		$out = $this->getOutput();
+		$request = $this->getRequest();
+		$this->setHeaders();
+		/**
+		 * only staff can operate this special page
+		 */
+		if ( !$wgUser->isAllowed( 'AddFestivalGift' ) ) {
+			$out->permissionRequired( 'AddFestivalGift' );
+			return;
+		}
+		$output = "";
+		$output .= "<form method='post' action='/wiki/special:reissuesystemgift?method=add' >
+			成就ID：<input type='text' name='giftId' >
+			用户名：<input type='text' name='user' >
+			<input class='mw-ui-button mw-ui-progressive' type='submit' value='添加'>
+			</form>";
+		$giftId = $request->getVal('giftId');
+		$userName = $request->getVal('user');
+		$method = $request->getVal('method');
+		$user = HuijiUser::newFromName($userName);
+		if ( $method == 'add' ) {
+			if ( is_numeric($giftId) && $user->getName() != null ) {
+				$usg = new UserSystemGifts( $user->getName() );
+				 if (HuijiFunctions::addLock( 'USG-Reissue-'.$user->getId(), 1 ) ){
+					$result = $usg->sendSystemGift( $giftId );
+				    HuijiFunctions::releaseLock('USG-Reissue-'.$user->getId());
+				    }
+				if( $result != false && $result !== null ){
+					$output .= "<h1>success</h1>";
+				}elseif(  $giftId == null || $userName == null ){
+					$output .= "<h1>输入错误</h1>";
+				}else{
+					$output .= "<h1>补发出错</h1>";
+				}
+			}else{
+				$output .= "<h1>输入有误</h1>";
+			}
+			
+		}
+		$out->addHTML( $output );
+	}
+
+
+}
