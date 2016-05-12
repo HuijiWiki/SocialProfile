b9286cdc37acc3df2bddf49e7fe18eb1cc5570e0
c86jeff <zhangjiexin0101@gmail.com>
2016-05-12 11:33:43 +0800

    improve curl function

--- UserEditRecord/curl.php
+++ UserEditRecord/curl.php
@@ -2,15 +2,17 @@
 class MySPCURL{
  
    
-   public static function postDataInJson($url, $data_string)
+   public static function postDataInJson($url, $method, $data_string)
 {
                
-                $header = array(
-                        'Content-Type: application/json',
-                        'Content-Length: '.strlen($data_string),
-                        );
+	
+	$address = $url.'/'.$method;
+        $header = array(
+               'Content-Type: application/json',
+               'Content-Length: '.strlen($data_string),
+        );
         $curl_opt_a = array(
-                CURLOPT_URL => $url,
+                CURLOPT_URL => $address,
                 CURLOPT_RETURNTRANSFER => 1,
                 CURLOPT_TIMEOUT => 30,
                 CURLOPT_POST => 1,
--- UserEditRecord/userEditRecord.php
+++ UserEditRecord/userEditRecord.php
@@ -53,35 +53,9 @@ function insertEditRecord($article, $rev, $baseID, $user ){
 	); 
 	if($wgIsProduction == false) return;       
 	require_once("curl.php");
-        $out =MySPCURL::postDataInJson('http://huijidata.com:8080/statisticQuery/webapi/edit/insertOnePageEditRecord',json_encode($log_data));
+        $out =MySPCURL::postDataInJson('http://huijidata.com:8080/statisticQuery/webapi/edit','insertOnePageEditRecord',json_encode($log_data));
 
 
 }
-
-/*
- function curl_post_json($data_string, $username, $pwd)
-{
-                $url =  'http://test.huiji.wiki:8080';
-                $header = array(
-                        'Content-Type: application/json',
-                        'Content-Length: '.strlen($data_string),
-                        );
-        $curl_opt_a = array(
-                CURLOPT_URL => $url,
-                CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_POST => 1,
-                CURLOPT_POSTFIELDS =>$data_string,
-                CURLOPT_HTTPHEADER =>$header,
-                CURLOPT_USERPWD => $username.':'.$pwd,
-        );
-        $ch = curl_init();
-        curl_setopt_array($ch,$curl_opt_a);
-        $out = curl_exec($ch);
-        curl_close($ch);
-
-        return $out;
-}
-*/
 
 ?>
