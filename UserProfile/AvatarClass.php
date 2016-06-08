af8baf08a1187e673b893ce54f6690124925649c
c86jeff <zhangjiexin0101@gmail.com>
2016-06-08 22:28:29 +0800

    change avatar path

--- UserProfile/AvatarClass.php
+++ UserProfile/AvatarClass.php
@@ -1,4 +1,5 @@
 <?php
+use OSS;
 /**
  * wAvatar class - used to display avatars
  * Example usage:
@@ -15,6 +16,8 @@ class wAvatar {
 	public $user_name = null;
 	public $user_id;
 	public $avatar_type = 0;
+	const AVATAR_BUCKET = "huiji-avatar";
+	protected $ossClient;
 
 	/**
 	 * Constructor
@@ -22,6 +25,18 @@ class wAvatar {
 	 * @param $size String: 's' for small, 'm' for medium, 'ml' for medium-large and 'l' for large
 	 */
 	function __construct( $userId, $size ) {
+		global $wgUseOss, $wgOssEndpoint;
+		if ($wgUseOss){
+            require_once "/var/www/html/Confidential.php";
+            $accessKeyId = Confidential::$aliyunKey;
+            $accessKeySecret = Confidential::$aliyunSecret;
+            $endpoint = $wgOssEndpoint;
+            try {
+                $this->ossClient = new Oss\OssClient($accessKeyId, $accessKeySecret, $endpoint);
+            } catch (Oss\OssException $e) {
+                wfErrorLog($e->getMessage(),'/var/log/mediawiki/SocialProfile.log');
+            }
+        }
 		$this->user_id = $userId;
 		$this->avatar_size = $size;
 	}
@@ -34,7 +49,7 @@ class wAvatar {
 	 *			image size (s, m, ml or l)
 	 */
 	function getAvatarImage() {
-		global $wgAvatarKey, $wgUploadDirectory, $wgMemc;
+		global $wgAvatarKey, $wgUploadDirectory, $wgMemc, $wgUseOss;
 
 		$key = wfForeignMemcKey( 'huiji', '', 'user', 'profile', 'avatar', $this->user_id, $this->avatar_size );
 		$data = $wgMemc->get( $key );
@@ -43,6 +58,34 @@ class wAvatar {
 		if ( $data ) {
 			$avatar_filename = $data;
 		} else {
+			if($wgUseOss){
+				
+                $bucket = self::AVATAR_BUCKET;
+                $avatar_filename = $wgAvatarKey . '_' . $this->user_id .  '_' . $this->avatar_size ;
+                $jpgDoesExist = $this->ossClient->doesObjectExist($bucket, $avatar_filename . ".jpg");
+                if ($jpgDoesExist){
+                	$avatar_filename .= ".jpg";
+                	$wgMemc->set( $key, $avatar_filename, 60 * 60 * 24 ); // cache for 24 hours
+                	return $avatar_filename;
+                }
+                $pngDoesExist = $this->ossClient->doesObjectExist($bucket, $avatar_filename . ".png");
+                if ($pngDoesExist){
+                	$avatar_filename .= ".png";
+                	$wgMemc->set( $key, $avatar_filename, 60 * 60 * 24 ); // cache for 24 hours
+                	return $avatar_filename;
+                }
+				$gifDoesExist = $this->ossClient->doesObjectExist($bucket, $avatar_filename . ".gif");  
+				if ($gifDoesExist){
+                	$avatar_filename .= ".gif";
+                	$wgMemc->set( $key, $avatar_filename, 60 * 60 * 24 ); // cache for 24 hours
+                	return $avatar_filename;
+				} 
+				$avatar_filename = 'default_' . $this->avatar_size . '.gif';
+				$wgMemc->set( $key, $avatar_filename, 60 * 60 * 24 ); // cache for 24 hours
+				return $avatar_filename;
+
+			}
+			// The old way without oss
 			$files = glob( $wgUploadDirectory . '/avatars/' . $wgAvatarKey . '_' . $this->user_id .  '_' . $this->avatar_size . "*" );
 			if ( !isset( $files[0] ) || !$files[0] ) {
 				$avatar_filename = 'default_' . $this->avatar_size . '.gif';
@@ -61,6 +104,10 @@ class wAvatar {
 	 *			image size (s, m, ml or l)
 	 */
 	function getAvatarUrlPath() {
+		global $wgUseOss, $wgOssAvatarPath;
+		if ($wgUseOss){
+			return $wgOssAvatarPath.'/'.$this->getAvatarImage();
+		}
 		return '/uploads/avatars/'.$this->getAvatarImage();
 	}
 
@@ -87,16 +134,18 @@ class wAvatar {
 	 * @return String: <img> HTML tag with full path to the avatar image
 	 * */
 	function getAvatarHtml( $extraParams = array() ) {
-		global $wgUploadPath;
+		global $wgUploadPath, $wgUseOss, $wgOssAvatarPath;
 		$user_id = $this->user_id;
 		$user = User::newFromId( $user_id );
 		$defaultParams = array(
 			'src' => "{$wgUploadPath}/avatars/{$this->getAvatarImage()}",
 			'alt' => 'avatar',
-			'border' => '0',
 			'class' => 'headimg',
 			'data-name' => $user->getName()
 		);
+		if ($wgUseOss){
+			$defaultParams['src'] =  "{$wgOssAvatarPath}/{$this->getAvatarImage()}";
+		}
 		$params = array_merge( $extraParams, $defaultParams );
 
 		return Html::element( 'img', $params, '' );
@@ -106,16 +155,18 @@ class wAvatar {
 	 * @return String: <a> HTML Anchor tag with full path to the avatar image
 	 * */
 	function getAvatarAnchor( $extraParams = array() ) {
-		global $wgUploadPath;
+		global $wgUploadPath, $wgUseOss, $wgOssAvatarPath;
 		$user_id = $this->user_id;
 		$user = User::newFromId( $user_id );
 		$defaultParams = array(
 			'src' => "{$wgUploadPath}/avatars/{$this->getAvatarImage()}",
 			'alt' => 'avatar',
-			'border' => '0',
 			'class' => 'headimg',
 			'data-name' => $user->getName()
 		);
+		if ($wgUseOss){
+			$defaultParams['src'] =  "{$wgOssAvatarPath}/{$this->getAvatarImage()}";
+		}
 		$params = array_merge( $extraParams, $defaultParams );
 		$linker = Linker::LinkKnown($user->getUserPage(), Html::element( 'img', $params, '' ));
 		return $linker;
