00fdb977791c55d971366f4000b6ae0a7ce050c7
c86jeff <zhangjiexin0101@gmail.com>
2016-06-08 22:28:14 +0800

    change avatar path

--- SiteStatus/SiteAvatarClass.php
+++ SiteStatus/SiteAvatarClass.php
@@ -21,7 +21,7 @@ class wSiteAvatar extends wAvatar{
 	 *			image size (s, m, ml or l)
 	 */
 	function getAvatarImage() {
-		global $wgSiteAvatarKey, $wgUploadDirectory, $wgMemc;
+		global $wgSiteAvatarKey, $wgUploadDirectory, $wgMemc, $wgUseOss;
 
 		$key = wfForeignMemcKey( 'huiji', '', 'site', 'profile', 'avatar', $this->user_id, $this->avatar_size );
 		$data = $wgMemc->get( $key );
@@ -30,6 +30,33 @@ class wSiteAvatar extends wAvatar{
 		if ( $data ) {
 			$avatar_filename = $data;
 		} else {
+			if($wgUseOss){
+				
+                $bucket = self::AVATAR_BUCKET;
+                $avatar_filename = $wgSiteAvatarKey . '_' . $this->user_id .  '_' . $this->avatar_size ;
+                $jpgDoesExist = $this->ossClient->doesObjectExist($bucket, $avatar_filename . ".jpg");
+                if ($jpgDoesExist){
+                	$avatar_filename .= ".jpg";
+                	$wgMemc->set( $key, $avatar_filename, 60 * 60 * 24 ); // cache for 24 hours
+                	return $avatar_filename;
+                }
+                $pngDoesExist = $this->ossClient->doesObjectExist($bucket, $avatar_filename . ".png");
+                if ($pngDoesExist){
+                	$avatar_filename .= ".png";
+                	$wgMemc->set( $key, $avatar_filename, 60 * 60 * 24 ); // cache for 24 hours
+                	return $avatar_filename;
+                }
+				$gifDoesExist = $this->ossClient->doesObjectExist($bucket, $avatar_filename . ".gif");  
+				if ($gifDoesExist){
+                	$avatar_filename .= ".gif";
+                	$wgMemc->set( $key, $avatar_filename, 60 * 60 * 24 ); // cache for 24 hours
+                	return $avatar_filename;
+				} 
+				$avatar_filename = 'site_default_' . $this->avatar_size . '.gif';
+				$wgMemc->set( $key, $avatar_filename, 60 * 60 * 24 ); // cache for 24 hours
+				return $avatar_filename;
+
+			}
 			$files = glob( $wgUploadDirectory . '/avatars/' . $wgSiteAvatarKey . '_' . $this->user_id .  '_' . $this->avatar_size . "*" );
 			if ( !isset( $files[0] ) || !$files[0] ) {
 				$avatar_filename = 'site_default_' . $this->avatar_size . '.png';
@@ -45,7 +72,7 @@ class wSiteAvatar extends wAvatar{
 	 * @return String: <img> HTML tag with full path to the avatar image
 	 * */
 	function getAvatarHtml( $extraParams = array() ) {
-		global $wgUploadPath;
+		global $wgUploadPath, $wgUseOss, $wgOssAvatarPath;
 		$site_prefix = $this->user_id;
 		$name = HuijiPrefix::prefixToSiteName( $site_prefix );
 		$defaultParams = array(
@@ -55,6 +82,9 @@ class wSiteAvatar extends wAvatar{
 			'class' => 'siteimg',
 			'data-name' => $name,
 		);
+		if ($wgUseOss){
+			$defaultParams['src'] =  "{$wgOssAvatarPath}/{$this->getAvatarImage()}";
+		}
 		$params = array_merge( $extraParams, $defaultParams );
 
 		return Html::element( 'img', $params, '' );
