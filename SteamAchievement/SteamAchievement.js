1cae5036adf14509653bb0c3b46cac13e3c82e1a
c86jeff <zhangjiexin0101@gmail.com>
2016-08-03 11:49:43 +0800

    steam js add more options

--- SteamAchievement/SteamAchievement.js
+++ SteamAchievement/SteamAchievement.js
@@ -6,13 +6,21 @@
 // 	mw.steamAchievement(['智慧祝福', '350640']);
 // });
 
-mw.__proto__.steamAchievement = function (member) {
-	if (mw.config.get('wgPageName') == member[0]) {
+mw.__proto__.steamAchievement = function (option) {
+	if (!option){
+		option = {};	
+	}
+	var config = {
+		page  = option.page || '',
+		key = option.key || '',
+		language = option.language || 'schinese'
+	}
+	if (config.key == '' || ( mw.config.get('wgPageName') === config.key && mw.config.get('wgAction') === 'View' ) ) {
 		var data = {
 			achievement: []
 		}
 		var myTemplate = mw.template.get('ext.socialprofile.steamachievement', 'SteamAchievement.mustache');
-		var API = 'http://steam.cdn.huijiwiki.com/ISteamUserStats/GetSchemaForGame/v2/?key=395E0A115E7986C8783646355E878444&appid=' + member[1] + '&l=schinese';
+		var API = 'http://steam.cdn.huijiwiki.com/ISteamUserStats/GetSchemaForGame/v2/?key=395E0A115E7986C8783646355E878444&appid=' + config.key + '&l=' + config.language;
 		var xmlHttp = new XMLHttpRequest();
 		xmlHttp.open("get", API, false);
 		xmlHttp.onreadystatechange = function() {
