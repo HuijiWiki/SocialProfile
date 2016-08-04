d3d87c016a43bc2158dda105469e3c62b034e624
c86jeff <zhangjiexin0101@gmail.com>
2016-08-04 10:47:22 +0800

    social profile style load sequence change

--- GlobalSearch/SpecialGlobalSearch.php
+++ GlobalSearch/SpecialGlobalSearch.php
@@ -39,7 +39,8 @@ class SpecialGlobalSearch extends SpecialPage {
 		// Set the page title, robot policies, etc.
 		$this->setHeaders();
 		$out->addModuleStyles('ext.socialprofile.globalsearch.css');
-		$out->addModules('skins.bootstrapmediawiki.search');
+		$out->addModulesStyles('skins.bootstrapmediawiki.search');
+		$out->addModulesScripts('skins.bootstrapmediawiki.search');
 		$output = "";
 		$output .= "<form method='get' class='form-inline' action='/wiki/special:globalsearch' >
 			<div id='mw-search-top-table'><input type='text' class='form-control' name='key' value='".$key."' >
