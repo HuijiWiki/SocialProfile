7fa64ddb47f2ab1e252dee8171fdbd7235d6e1e4
Reasno <guxi99@gmail.com>
2016-11-07 15:30:48 +0800

    add a new job type: asyncEventJob

---
+++ UserEditToUpsert/AsyncEventJob.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * A temporary fix. Needs clean up.
+ */
+class AsyncEventJob extends Job {
+	public function __construct( $title, $params ) {
+		// Replace synchroniseThreadArticleData with an identifier for your job.
+		parent::__construct( 'asyncEventJob', $title, $params );
+	}
+
+	/**
+	 * Execute the job
+	 *
+	 * @return bool
+	 */
+	public function run() {
+		switch ($this->params[0]) {
+		 	case 'entrytran_save':
+				// Load data from $this->params and $this->title
+				list($type, $article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId) = $this->params;
+				return $this->saveEntryTran($article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId);
+		 		break;
+		 	case 'entrytran_undelete':
+		 		list($type, $title, $revision, $oldPageId) = $this->params;
+ 				return $this->unDeleteEntryTran( $title, $revision, $oldPageId );
+		 		break;
+		 	case 'es_upsertpage':
+		 		list($type, $title, $rev) = $this->params;
+		 		return $this->upsertPage($title, $rev);
+		 	case 'es_undeletepage':
+		 		list($type, $title, $revision, $oldPageId ) = $this->params;
+		 		return $this->unDeletePage($title, $revision, $oldPageId);
+		 	default:
+		 		# code...
+		 		break;
+		 } 
+	
+
+		return true;
+	}
+	public function saveEntryTran($article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId){
+	    if($article == null || $revision == null || $article->getTitle() == null) return true;
+	    
+		$logger = MediaWiki\Logger\LoggerFactory::getInstance( 'updateEntryTran' );
+	    $logger->debug( "parsing begins at ".time(), [$article, $content] );
+		$links= [];
+		try{
+			$parserOutput = $content->getParserOutput($article->getTitle());
+			$links = $parserOutput->getLanguageLinks();
+			$logger->debug( "parsing ends at ".time(), [$article, $content] );
+		} catch(Exception $e){
+			$logger->error($e->getMessage());
+			exit();
+		}
+		if(count($links) == 0){
+			upsert("", $article->getTitle()->getText(),"");
+		}
+	 	
+		$redirectToPage = $content->getRedirectTarget();
+        //redirectPageTitle
+        $toTitle = $redirectToPage != null ? $redirectToPage->getText():$article->getTitle()->getText();
+
+		$preRev = $revision->getPrevious();	
+		if($preRev == null){
+			insert($toTitle, $article->getTitle()->getText(),$links);
+			return;
+		}else{
+			$preLinks = [];
+			try{
+				$content = $preRev->getContent(Revision::RAW);
+				$parserOutput = $content->getParserOutput($article->getTitle());
+				$preLinks = $parserOutput->getLanguageLinks();
+			} catch(Exception $e){
+				$logger->error($e->getMessage());
+				exit();
+			}
+
+			if(count($links) != count($preLinks)){
+				insert($toTitle,$article->getTitle()->getText(),$links);
+				return true;
+			}
+
+			$preSet = [];
+			$set = [];
+			
+			foreach ( $preLinks as $link) {
+	  	        	list( $key, $title ) = explode( ':', $link, 2 );
+	               		$preSet[$key] = $title;
+			}
+
+			foreach ( $links as $link) {
+	  	        	list( $key, $title ) = explode( ':', $link, 2 );
+				if($preSet[$key] == null || $preSet[$key] != $title){
+					insert($toTitle,$article->getTitle()->getText(),$links);
+					return true;
+				}
+			}
+
+	    }   		
+	}
+	public function unDeleteEntryTran($title, $revision, $oldPageId){
+		if($title == null || $title->getNamespace() !== 0) return;	
+		$logger = MediaWiki\Logger\LoggerFactory::getInstance( 'updateEntryTran' );
+		$links= [];
+		try{
+			$content = $revision->getContent(Revision::RAW);
+			$redirectTitleObject = $content->getRedirectTarget(); 
+			$toTitle = $redirectTitleObject != null ? $redirectTitleObject->getText():$title->getText();
+			$logger->debug( "parsing begins at ".time(), [$article, $content] );
+			$parserOutput = $content->getParserOutput($title);
+			$logger->debug( "parsing ends at ".time(), [$article, $content] );
+			$links = $parserOutput->getLanguageLinks();
+		} catch(Exception $e){
+			$logger->error($e->getMessage());
+			exit();
+		}
+		if(count($links) >0 ){
+			insert($toTitle,$title->getText(),$links);
+			return true;
+		}
+	}
+	public function upsertPage($title, $rev){
+		global $wgHuijiPrefix, $wgSitename,$wgIsProduction;
+		$logger = MediaWiki\Logger\LoggerFactory::getInstance( 'updateESContent' );
+		if($wgIsProduction == false) return true;
+		if($rev == null || $title == null || $title->getNamespace() !== 0) return true;
+		$old_rev = $rev->getPrevious();
+		$old_redirect = null;
+		$new_redirect = null;
+		$old_redirectId = -1;
+		$new_redirectId = -1;
+		$category = array();
+		//new & old content
+		if($old_rev != null && ($old_content = $old_rev->getContent(Revision::RAW)) != null) $old_redirect = $old_content->getRedirectTarget();
+		if(($new_content = $rev->getContent(Revision::RAW)) != null) $new_redirect = $new_content->getRedirectTarget();
+
+		//new & old redirect 
+	         
+		if($old_redirect != null){
+			$old_redirectId = $old_redirect->getArticleID();
+		}else{
+			$old_redirectId = -1;
+		}
+
+		if($new_redirect != null){
+			$new_redirectId = $new_redirect->getArticleID();
+		}else{
+			$new_redirectId = -1;
+		}
+		$logger->debug('parser begins at '.time(), [$title, $rev]);
+		//category
+		$options = $new_content->getContentHandler()->makeParserOptions( 'canonical' );
+	    $output = $new_content->getParserOutput( $title, $rev->getId(), $options,true);
+	    $category = array_map( 'strval', array_keys( $output->getCategories() ) );
+
+	    $logger->debug('parser ends at '.time(), [$title, $rev]);
+
+		$titleName = ($title->getText() == "首页") ? $wgSitename : $title->getText();
+		$preTitle = $old_rev != null ? $old_rev->getTitle()->getText():null;
+		$redirectPageTitle = $new_redirect != null ? $new_redirect->getText():null;
+		$post_data = array(
+			'timestamp' => $rev->getTimestamp(),
+			'content' => $output->getText(),
+			'sitePrefix' => $wgHuijiPrefix,
+			'siteName' => $wgSitename,
+			'id' => $title->getArticleID(),
+			'title' => $titleName,
+			'preTitle' => $preTitle,
+			'preRedirectPageId' => $old_redirectId,
+			'redirectPageId' => $new_redirectId,
+			'category' => $category,
+			'redirectPageTitle' => $redirectPageTitle,
+			
+		);
+		$post_data_string = json_encode($post_data);
+	//	wfErrorLog($post_data_string,"/var/log/mediawiki/SocialProfile.log");
+		curl_post_json('upsert',$post_data_string);
+		return true;
+	}
+	public function unDeletePage($title, $revision, $oldPageId){
+		global $wgHuijiPrefix, $wgSitename,$wgIsProduction;
+		$logger = MediaWiki\Logger\LoggerFactory::getInstance( 'updateESContent' );
+		if($wgIsProduction == false) return true;	
+		//title
+		if($title == null || $title->getNamespace() !== 0) return;
+		$titleT = ($title->getText() == "首页") ? $wgSitename : $title->getText();
+		// new_content ,   new_redirect 
+		if(($new_content = $revision->getContent(Revision::RAW)) != null) $new_redirect = $new_content->getRedirectTarget();
+		//redirectPageTitle
+		$redirectPageTitle = $new_redirect != null ? $new_redirect->getText():null;
+		//new_redirectId
+		if($new_redirect != null){
+			$new_redirectId = $new_redirect->getArticleID();
+		}else{
+			$new_redirectId = -1;
+		}
+
+		//category
+		$logger->debug('parser begins at '.time(), [$title, $revision]);
+		$options = $new_content->getContentHandler()->makeParserOptions( 'canonical' );
+	       	$output = $new_content->getParserOutput( $title, $revision->getId(), $options,true);
+	       	$category = array_map( 'strval', array_keys( $output->getCategories() ) );
+
+	    $logger->debug('parser ends at '.time(), [$title, $revision]);
+		$post_data = array(
+			'timestamp' => $revision->getTimestamp(),
+			'content' => $output->getText(),
+			'sitePrefix' => $wgHuijiPrefix,
+			'siteName' => $wgSitename,
+			'id' => $title->getArticleID(),
+			'title' => $titleT,
+			'preTitle' => null,
+			'preRedirectPageId' => -1,
+			'redirectPageId' => $new_redirectId,
+			'category' => $category,
+			'redirectPageTitle' => $redirectPageTitle,
+			
+		);
+		$post_data_string = json_encode($post_data);
+	//	wfErrorLog($post_data_string,"/var/log/mediawiki/SocialProfile.log");
+		curl_post_json('upsert',$post_data_string);
+		return true;
+	}
+}
