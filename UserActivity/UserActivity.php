33b24c7e3f47d6da2bbdd5bad326f83ab62b74a4
Reasno <guxi99@gmail.com>
2016-11-24 15:40:28 +0800

    skins

--- UserActivity/UserActivity.php
+++ UserActivity/UserActivity.php
@@ -30,10 +30,10 @@ $wgExtensionCredits['specialpage'][] = array(
 
 // Set up the new special page
 $wgMessagesDirs['UserActivity'] = __DIR__ . '/i18n';
-// $wgAutoloadClasses['UserActivity'] = __DIR__ . '/UserActivityClass.php';
-// $wgAutoloadClasses['UserHome'] = __DIR__ . '/UserActivity.body.php';
+$wgAutoloadClasses['UserActivity'] = __DIR__ . '/UserActivityClass.php';
+$wgAutoloadClasses['UserHome'] = __DIR__ . '/UserActivity.body.php';
 $wgAutoloadClasses['UserActivity2'] = __DIR__.'/UserActivity/UserActivity2Class.php';
-// $wgSpecialPages['UserActivity'] = 'UserHome';
+$wgSpecialPages['UserActivity'] = 'UserHome';
 
 // Register the CSS with ResourceLoader
 $wgResourceModules['ext.socialprofile.useractivity.css'] = array(
